50c39949828a16c519b57f1076dc2270162bd738
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2025-01-08 17:40:27 +0800

    ~72223

--- readme.md
+++ readme.md
@@ -1,21 +1,26 @@
 #
 
-unclefans.com////suit/detail
+angryanut.com////suit/detail
 
 ## stat
 |start|end|
 |-:|-:|
-|1	|	71877 |
+|1	|	72223 |
 |100000000	| 136353001 |
-|200000000	| 235460501 |
+|200000000	| 236649101 |
 
 ## replace
+```regexp
 ((,(\n\t+)?"(current_sources|finish_sources|current_activity|next_activity|unlock_items|activity_entrance|items)":null|,(\n\t+)?"activity_entrance":\{(\n\t+)?"id":0,(\n\t+)?"item_id":0,(\n\t+)?"title":"",(\n\t+)?"image_cover":""(,(\n\t+)?"jump_link":"")?(\n\t+)?\}|,(\n\t+)?"fan_user":\{(\n\t+)?"mid":0,(\n\t+)?"nickname":"",(\n\t+)?"avatar":""(\n\t+)?\}|,(\n\t+)?"suit_items":\{\}|,(\n\t+)?"(item_stock_surplus|sale_count_desc|ref_mid)":"\d{0,}[千万]?\+?"|,(\n\t+)?"(time_left|time_left_month|sale_time_end|sale_left_time|sale_surplus|sales_mode)":-?\d+|(\n\t+)?,"(sale_count_desc|total_count_desc|jump_link|tag|tag_month)":"")+|("(open_platform_vip_discount|hot|gray_rule)":"(true|false)",(\n\t+)?|"(sale_count_desc|total_count_desc|jump_link|tag|tag_month)":"",(\n\t+)?|"(tag|tag_month)":"(新品|即将售罄|正在预约|即将下架|大会员平台折扣|粉丝套装已售罄|大会员限时折扣|即将开售)?",(\n\t+)?|"gray_rule_type":"all",(\n\t+)?|"state":"(in)?active",(\n\t+)?)+|,(\n\t+)?"(current_activity|next_activity)":\{(\n\t+)?"type":"(vip_discount|open_platform_vip_discount)",(\n\t+)?"time_limit":true,((\n\t+)?"time_left":-?\d+,)?((\n\t+)?"tag":"(新品|即将售罄|正在预约|即将下架|大会员平台折扣|粉丝套装已售罄|大会员限时折扣|即将开售)?",)?(\n\t+)?"price_bp_month":\d+,(\n\t+)?"price_bp_forever":\d+,(\n\t+)?"type_month":"(vip_discount|open_platform_vip_discount)"(,(\n\t+)?"tag_month":"(新品|即将售罄|正在预约|即将下架|大会员平台折扣|粉丝套装已售罄|大会员限时折扣|即将开售)?")?,(\n\t+)?"time_limit_month":true(,(\n\t+)?"time_left_month":-?\d+)?(\n\t+)?\})+
 
 \?e=[0-9a-zA-Z_=]{70,}(&|\\\\u0026)uipk=\d+(&|\\\\u0026)nbs=\d+(&|\\\\u0026)deadline=\d+(&|\\\\u0026)gen=playurlv2(&|\\\\u0026)os=(08cbv|cosbv|alibv|upos|bdbv)(&|\\\\u0026)oi=\d+(&|\\\\u0026)trid=[0-9a-fA-F]{31,33}(&|\\\\u0026)mid=\d+(&|\\\\u0026)platform=html5(&|\\\\u0026)og=(cos|hw)(&|\\\\u0026)upsig=[0-9a-f]{32}(&|\\\\u0026)uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform,og(&|\\\\u0026)bvc=vod(&|\\\\u0026)nettype=\d+(&|\\\\u0026)orderid=\d,\d(&|\\\\u0026)logo=\d+(&|\\\\u0026)f=B_0_0
 
+__bilivideo__
+http(s)?://(upos-(sz|tribe)-(mirror|static|estg)(08c|cos|ali|oss|bd)?(-cmask)?|data|bvc|(c|d)\d+--(p\d+--)?(cn|ov|tf)-gotcha\d+|cn-[a-z]+-(cm|ct|cc|fx|se|gd|cu|eq|ix|wasu)(-v)?-\d+)\.bilivideo\.com
+```
+
 ## find
-"(activity_entrance|current_activity|current_sources|finish_sources|gray_rule|gray_rule_type|hot|item_stock_surplus|items|jump_link|next_activity|open_platform_vip_discount|sale_count_desc|sale_left_time|sale_surplus|sale_time_end|sales_mode|state|suit_items|tag|tag_month|time_left|time_left_month|total_count_desc|unlock_items)"
+"(activity_entrance|current_activity|current_sources|finish_sources|gray_rule|gray_rule_type|hot|item_stock_surplus|jump_link|next_activity|open_platform_vip_discount|sale_count_desc|sale_left_time|sale_surplus|sale_time_end|sales_mode|state|tag|tag_month|time_left|time_left_month|total_count_desc|unlock_items)"
 (noface|账号已注销)
 &(e|uipk|nbs|deadline|gen|playurlv2|os|08cbv|cosbv|alibv|upos|bdbv|oi|trid|platform|og|upsig|cos|hw|uparams|bvc|vod|nettype|orderid|logo|xxx)=
 
---
+++ sort_ids.py
@@ -0,0 +1,209 @@
+from enum import StrEnum
+import json
+import sys
+
+_EMPTY_FAN_USER = {"mid": 0, "nickname": "", "avatar": ""}
+_EMPTY_ACTIVITY_ENTRANCE = {"id": 0, "item_id": 0, "title": "", "image_cover": "", "jump_link": ""}
+
+
+def _sort_str_list(s: str) -> str:
+    """example: '1,3,2,4,5' -> '1,2,3,4,5'"""
+    if s.count(",") == 0:
+        return s
+    a = json.loads(f"[{s}]")
+    b = sorted(a)
+    return ",".join(str(c) for c in b)
+
+
+def _sort_list_dict(l: list[dict], k1: str, k2: str) -> list[dict]:
+    items_with_k1 = [item for item in l if item[k1] not in [0, "0"]]
+    items_with_k2 = [item for item in l if item[k1] in [0, "0"]]
+    items_with_k1.sort(key=lambda x: x[k1])
+    items_with_k2.sort(key=lambda x: x[k2])
+    return items_with_k1 + items_with_k2
+
+
+def _sort_p6_emoji(l: list[dict]) -> list[dict]:
+    for i in range(len(l)):
+        if isinstance(l[i].get("properties"), dict):
+            if isinstance(l[i]["properties"].get("item_ids"), str):
+                l[i]["properties"]["item_ids"] = _sort_str_list(l[i]["properties"]["item_ids"])
+        l[i]["items"] = _sort_list_dict(l[i]["items"], "item_id", "name")
+    return l
+
+
+def _del_keys_anyway(d: dict, k: str, r=True):
+    if k in d:
+        d.pop(k)
+    for key in d:
+        if isinstance(d[key], dict):
+            _del_keys_anyway(d[key], k, r)
+        elif isinstance(d[key], list):
+            for item in d[key]:
+                if isinstance(item, dict):
+                    _del_keys_anyway(item, k, r)
+
+
+def _del_keys_if_eq_value(d: dict, k: str, v, r=True):
+    if k in d and d.get(k) == v:
+        d.pop(k)
+    if not r:
+        return
+    for key in d:
+        if isinstance(d[key], dict):
+            _del_keys_if_eq_value(d[key], k, v)
+        if isinstance(d[key], list):
+            for item in d[key]:
+                if isinstance(item, dict):
+                    _del_keys_if_eq_value(item, k, v)
+
+
+class OPR(StrEnum):
+    EQ = "=="
+    NEQ = "!="
+    GT = ">"
+    LT = "<"
+    GEQ = ">="
+    LEQ = "<="
+
+
+def _del_keys_if_int_cmp(d: dict, k: str, v: int, opr: OPR, r=True):
+    if k in d and isinstance(d[k], int):
+        match opr:
+            case OPR.EQ:
+                if d.get(k) == v:
+                    d.pop(k)
+            case OPR.GT:
+                if d.get(k) > v:  # type: ignore[reportOptionalOperand]
+                    d.pop(k)
+            case OPR.LT:
+                if d.get(k) < v:  # type: ignore[reportOptionalOperand]
+                    d.pop(k)
+            case OPR.GEQ:
+                if d.get(k) >= v:  # type: ignore[reportOptionalOperand]
+                    d.pop(k)
+            case OPR.LEQ:
+                if d.get(k) <= v:  # type: ignore[reportOptionalOperand]
+                    d.pop(k)
+            case OPR.NEQ:
+                if d.get(k) != v:
+                    d.pop(k)
+    if not r:
+        return
+    for key in d:
+        if isinstance(d[key], dict):
+            _del_keys_if_int_cmp(d[key], k, v, opr)
+        if isinstance(d[key], list):
+            for item in d[key]:
+                if isinstance(item, dict):
+                    _del_keys_if_int_cmp(item, k, v, opr)
+
+
+def _main(path: str):
+    with open(path, "r", encoding="utf-8") as fp:
+        item: dict = json.load(fp)
+    s1 = len(item)
+    if item.get("suit_items") == None:
+        return
+    print(item["item_id"])
+    match item["part_id"]:
+        case 1:
+            ...
+        case 2:
+            ...
+        case 3:
+            ...
+        case 4:
+            ...
+        case 5:
+            if isinstance(item.get("properties"), dict):
+                if isinstance(item["properties"].get("item_ids"), str):
+                    item["properties"]["item_ids"] = _sort_str_list(item["properties"]["item_ids"])
+            if isinstance(item.get("suit_items"), dict):
+                if isinstance(item["suit_items"].get("emoji"), list):
+                    item["suit_items"]["emoji"] = _sort_list_dict(item["suit_items"]["emoji"], "item_id", "name")
+        case 6:
+            if isinstance(item.get("properties"), dict):
+                if isinstance(item["properties"].get("fan_item_ids"), str):
+                    item["properties"]["fan_item_ids"] = _sort_str_list(item["properties"]["fan_item_ids"])
+            if isinstance(item.get("suit_items"), dict):
+                if isinstance(item["suit_items"].get("card"), list):
+                    item["suit_items"]["card"] = _sort_list_dict(item["suit_items"]["card"], "item_id", "name")
+                if isinstance(item["suit_items"].get("card_bg"), list):
+                    item["suit_items"]["card_bg"] = _sort_list_dict(item["suit_items"]["card_bg"], "item_id", "name")
+                if isinstance(item["suit_items"].get("loading"), list):
+                    item["suit_items"]["loading"] = _sort_list_dict(item["suit_items"]["loading"], "item_id", "name")
+                if isinstance(item["suit_items"].get("pendant"), list):
+                    item["suit_items"]["pendant"] = _sort_list_dict(item["suit_items"]["pendant"], "item_id", "name")
+                if isinstance(item["suit_items"].get("play_icon"), list):
+                    item["suit_items"]["play_icon"] = _sort_list_dict(item["suit_items"]["play_icon"], "item_id", "name")
+                if isinstance(item["suit_items"].get("skin"), list):
+                    item["suit_items"]["skin"] = _sort_list_dict(item["suit_items"]["skin"], "item_id", "name")
+                if isinstance(item["suit_items"].get("space_bg"), list):
+                    item["suit_items"]["space_bg"] = _sort_list_dict(item["suit_items"]["space_bg"], "item_id", "name")
+                if isinstance(item["suit_items"].get("thumbup"), list):
+                    item["suit_items"]["thumbup"] = _sort_list_dict(item["suit_items"]["thumbup"], "item_id", "name")
+                if isinstance(item["suit_items"].get("emoji_package"), list):
+                    item["suit_items"]["emoji_package"] = _sort_p6_emoji(item["suit_items"]["emoji_package"])
+        case 7:
+            ...
+        case 8:
+            ...
+        case 9:
+            ...
+        case 10:
+            ...
+        case 11:
+            ...
+        case 12:
+            ...
+        case 13:
+            ...
+        case _:
+            ...
+    _del_keys_if_eq_value(item, "activity_entrance", _EMPTY_ACTIVITY_ENTRANCE, False)
+    _del_keys_if_eq_value(item, "fan_user", _EMPTY_FAN_USER, False)
+    _del_keys_if_eq_value(item, "suit_items", {})
+
+    _del_keys_anyway(item, "current_activity")
+    _del_keys_anyway(item, "current_sources")
+    _del_keys_anyway(item, "finish_sources")
+    _del_keys_anyway(item, "gray_rule_type")
+    _del_keys_anyway(item, "gray_rule")
+    _del_keys_anyway(item, "hot")
+    _del_keys_anyway(item, "item_stock_surplus")
+    _del_keys_anyway(item, "next_activity")
+    _del_keys_anyway(item, "open_platform_vip_discount")
+    _del_keys_anyway(item, "sale_count_desc")
+    _del_keys_anyway(item, "sale_left_time")
+    _del_keys_anyway(item, "sale_surplus")
+    _del_keys_anyway(item, "state")
+    _del_keys_anyway(item, "tag")
+    _del_keys_anyway(item, "total_count_desc")
+
+    _del_keys_if_eq_value(item, "activity_entrance", None, False)
+    _del_keys_if_eq_value(item, "associate_words", "")
+    _del_keys_if_eq_value(item, "items", None)
+    _del_keys_if_eq_value(item, "jump_link", "")
+    _del_keys_if_eq_value(item, "ref_mid", "0")
+    _del_keys_if_eq_value(item, "sales_mode", 0)
+    _del_keys_if_eq_value(item, "suit_item_id", 0)
+    _del_keys_if_eq_value(item, "unlock_items", None)
+    _del_keys_if_int_cmp(item, "sale_time_end", 0, OPR.LEQ)
+
+    if s1 != len(item):
+        print("WARN:", item["item_id"])
+    with open(path, "w", encoding="utf-8") as fp:
+        json.dump(item, fp, ensure_ascii=False, separators=(",", ":"), indent="\t")
+
+
+if __name__ == "__main__":
+    try:
+        files = sys.argv[1:]
+        for file in files:
+            _main(file)
+    finally:
+        pass
+    import time
+
+    time.sleep(10)
