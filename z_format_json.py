18c4ba3305549aaa0396390eb53e6157a9c5e30b
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2025-01-08 17:40:27 +0800

    ~72223

---
+++ z_format_json.py
@@ -0,0 +1,23 @@
+import json
+import sys
+
+
+def _main(path: str):
+    with open(path, "r", encoding="utf-8") as fp:
+        item: dict = json.load(fp)
+
+    with open(path, "w", encoding="utf-8") as fp:
+        json.dump(item, fp, ensure_ascii=False, separators=(",", ":"), indent="\t")
+
+
+if __name__ == "__main__":
+    try:
+        files = sys.argv[1:]
+        for file in files:
+            _main(file)
+    except Exception as e:
+        import time
+
+        time.sleep(10)
+    finally:
+        pass
