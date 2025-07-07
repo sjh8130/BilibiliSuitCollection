fe774049e545e9a27aecc17e80954d0bb11b85ef
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2025-07-07 10:05:40 +0800

    u

---
+++ 🗑.py
@@ -0,0 +1,35 @@
+import csv
+import json
+from pathlib import Path
+
+_M = Path.cwd().resolve()
+TRASH = "🗑"
+IDCSV = "ids.csv"
+j = _M / "🗑.json"
+
+
+def _O():
+    a = []
+    b = []
+    c = _M / IDCSV
+    with c.open(encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=",")
+        next(reader)
+        for f in reader:
+            if f[1] == TRASH:
+                b.append(int(f[0]))
+            else:
+                a.append(int(f[0]))
+    return a, b
+
+
+def main():
+    a = json.loads(j.read_text("utf-8"))
+    b, c = _O()
+    for d in c:
+        a.append(d)
+    j.write_text(json.dumps(sorted(set(a)), indent="\t"))
+
+
+if __name__ == "__main__":
+    main()
