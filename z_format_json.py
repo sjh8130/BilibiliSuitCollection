4604bcb06c88ed416b1310304834d212e406767e
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2026-06-11 08:42:45 +0800

    0,4,e,u

--- z_format_json.py
+++ z_format_json.py
@@ -17,7 +17,12 @@ if __name__ == "__main__":
     try:
         files = sys.argv[1:]
         for file in tqdm.tqdm(files):
-            _main(Path(file))
+            p = Path(file)
+            if p.is_dir():
+                for pp in p.iterdir():
+                    _main(pp)
+            else:
+                _main(p)
     except Exception as e:  # noqa: BLE001
         import time
 
